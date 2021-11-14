c658b4c98801365ea842d4d2cb7ec5ce7acfa85d
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-11-14 18:17:51 -0500

    Front end file structure complete

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Layout from './Layout/Layout'
+import CarDisplay from './CarDisplay/CarDisplay'
+import CarShopping from './CarShopping/CarShopping'
+import ShoppingCart from './ShoppingCart/ShoppingCart'
+export { Layout, CarDisplay, CarShopping, ShoppingCart }
+
