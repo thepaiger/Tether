de33b4358e4a0e4d66670640d6bfbb769c82bec5
tyrus <tyrushenry03@gmail.com>
2021-11-15 16:46:31 -0500

    carousel buttons icons added + Home tweaks

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,16 +1,20 @@
 import './Home.css'
 import { Layout, ProductCards } from '../../components'
 import { NavLink } from 'react-router-dom'
+// import Carousel from '../../components/Carousel/Carousel'
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
+
+      {/* <Carousel /> */}
+
       <div className='welcome-browse'>
         <h1>WELCOME TO THE FUTURE</h1>
         <NavLink to="/inventory">Browse Inventory</NavLink>
       </div>
       <div className="home-logo">
-        <img src={'/images/logo.png'} alt="Logo" className="logo" />
+        <img src={'/images/logo.png'} alt="Logo" className="home-logo" />
         <p>Your one-stop shop for Electric Supercars</p>
       </div>
     </Layout>
