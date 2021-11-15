d7935aaa959df9281180652349f43d2e2b0ed54a
tyrus <tyrushenry03@gmail.com>
2021-11-15 15:48:04 -0500

    Home text

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,8 +1,19 @@
+import './Home.css'
+import { Layout, ProductCards } from '../../components'
+import { NavLink } from 'react-router-dom'
 
-
-const Home = ({ }) => {
+const Home = (props) => {
   return (
-    <div>hi</div>
+    <Layout user={props.user}>
+      <div className='welcome-browse'>
+        <h1>WELCOME TO THE FUTURE</h1>
+        <NavLink to="/inventory">Browse Inventory</NavLink>
+      </div>
+      <div className="home-logo">
+        <img src={'/images/logo.png'} alt="Logo" className="logo" />
+        <p>Your one-stop shop for Electric Supercars</p>
+      </div>
+    </Layout>
   )
 }
 
