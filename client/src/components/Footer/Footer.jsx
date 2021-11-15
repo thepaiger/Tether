369f0ee0fe868e9b2d9cb9229db3debe17b5d210
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-11-15 10:59:24 -0500

    footer complete

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+import { NavLink } from 'react-router-dom'
+import { ExternalLink } from 'react-router-dom'
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <p className="copyright">Copyright © 2021</p>
+
+        <a href="https://github.com/DarrylFalls/Tether" className="github-footer"><img src={'/images/icons/github.svg'}></img></a>
+      </div>
+    </footer>
+  )
+}
