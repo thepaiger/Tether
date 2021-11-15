d114d36a3967725a8d9fcc2a1e042d5e9ea9f71f
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-11-15 09:38:02 -0500

    Nav componnt complete

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -16,3 +16,20 @@ const alwaysOptions = (
     <NavLink className="link" to="/models">MODELS</NavLink>
   </>
 )
+const Nav = ({ user }) => {
+  return (
+    <nav>
+      <div className="nav">
+        <NavLink className="logo" to="/">
+          -logo here-</NavLink>
+        <div className="links">
+          {user && <div className="link-welcome">Welcome, {user.username}</div>}
+          {alwaysOptions}
+          {user ? authenticatedOptions : unauthenticadedOptions}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
