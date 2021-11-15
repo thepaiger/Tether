b27cf1457d83f79e82c9eff85b9c96735aeaadd0
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-11-15 14:51:38 -0600

    added required, placeholders, type, & htmlFor/id's to signIn/signUp forms

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,47 +1,65 @@
-import { Navigate } from 'react-router-dom'
-import { useState } from 'react'
-import { signIn } from '../../services/users'
-
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+import { signIn } from "../../services/users";
 
 const SignIn = ({ user, setUser, loggedIn, setLoggedIn }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [navigateToggle, setNavigateToggle] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [navigateToggle, setNavigateToggle] = useState(false);
 
   const handleSubmit = async (ev) => {
-    ev.preventDefault()
+    ev.preventDefault();
     try {
       const form = {
         email,
-        password
-      }
-      const user = await signIn(form)
-      setUser(user)
-      setNavigateToggle(true)
+        password,
+      };
+      const user = await signIn(form);
+      setUser(user);
+      setNavigateToggle(true);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   if (navigateToggle) {
-    return <Navigate to="/" />
+    return <Navigate to="/" />;
   }
 
   return (
-    <div classname='sign-in-display-div'>
-      <div classname='sign-in-div'>
-        <form classname='sign-in-form' onSubmit={(ev) => handleSubmit(ev)} >
-          <label classname='sign-in-email-label'>Email:</label>
-          <input classname='sign-in-email-input' value={email} onChange={(ev) => setEmail(ev.target.value)} />
+    <div classname="sign-in-display-div">
+      <div classname="sign-in-div">
+        <form classname="sign-in-form" onSubmit={(ev) => handleSubmit(ev)}>
+          <label classname="sign-in-email-label" htmlFor="sign-in-email-input">Email:</label>
+          <input
+            classname="sign-in-email-input"
+            id="sign-in-email-input"
+            type="email"
+            value={email}
+            placeholder="email"
+            onChange={(ev) => setEmail(ev.target.value)}
+            required
+          />
           <br />
-          <label classname='sign-in-password-label'>Password:</label>
-          <input classname='sign-in-password-input' value={password} onChange={(ev) => setPassword(ev.target.value)} />
+          <label classname="sign-in-password-label" htmlFor="sign-in-password-input">Password:</label>
+
+          {/* update type to password once testing complete */}
+          <input
+            classname="sign-in-password-input"
+            id="sign-in-password-input"
+            type="text"
+            value={password}
+            placeholder="********"
+            onChange={(ev) => setPassword(ev.target.value)}
+            required
+          />
           <br />
-          <input type='submit' value='Sign In' />
+
+          <input type="submit" value="Sign In" />
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,94 +1,152 @@
-
-import { useState } from 'react'
-import { Navigate } from 'react-router-dom'
-import { signUp } from '../../services/users'
-
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { signUp } from "../../services/users";
 
 const SignUp = ({ user, setUser, loggedIn, setLoggedIn }) => {
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirm, setConfirm] = useState('')
-  const [match, setMatch] = useState(false)
-  const [matchToggle, setMatchToggle] = useState(false)
-  const [lengthToggle, setLengthToggle] = useState(false)
-  const [navigateToggle, setNavigateToggle] = useState(false)
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [match, setMatch] = useState(false);
+  const [matchToggle, setMatchToggle] = useState(false);
+  const [lengthToggle, setLengthToggle] = useState(false);
+  const [navigateToggle, setNavigateToggle] = useState(false);
 
   const handleSubmit = async (ev) => {
-    ev.preventDefault()
+    ev.preventDefault();
     if (match === true) {
       try {
         const form = {
           name,
           email,
           password,
-          "shopping_cart": []
-        }
-        const user = await signUp(form)
-        setUser(user)
-        setLoggedIn(true)
-        setNavigateToggle(true)
+          shopping_cart: [],
+        };
+        const user = await signUp(form);
+        setUser(user);
+        setLoggedIn(true);
+        setNavigateToggle(true);
       } catch (error) {
-        console.error(error)
-        setEmail('')
-        setName('')
-        setPassword('')
-        setConfirm('')
+        console.error(error);
+        setEmail("");
+        setName("");
+        setPassword("");
+        setConfirm("");
       }
     }
-  }
+  };
 
   if (navigateToggle) {
-    return <Navigate to="/" />
+    return <Navigate to="/" />;
   }
 
   const checkPassword = () => {
     if (password.length > 0 && password.length < 8) {
-      setLengthToggle(true)
+      setLengthToggle(true);
     } else if (password === confirm && confirm.length > 7) {
-      setMatch(true)
-      setMatchToggle(false)
-      setLengthToggle(false)
+      setMatch(true);
+      setMatchToggle(false);
+      setLengthToggle(false);
     } else if (password !== confirm && confirm.length === 0) {
-      setMatch(false)
-      setMatchToggle(false)
-      setLengthToggle(false)
+      setMatch(false);
+      setMatchToggle(false);
+      setLengthToggle(false);
     } else if (password !== confirm && confirm.length > 7) {
-      setMatch(false)
-      setMatchToggle(true)
-      setLengthToggle(false)
+      setMatch(false);
+      setMatchToggle(true);
+      setLengthToggle(false);
     }
-  }
+  };
 
   const handleChange = (ev) => {
-    setConfirm(ev.target.value)
-    checkPassword()
-  }
+    setConfirm(ev.target.value);
+    checkPassword();
+  };
 
   return (
-    <div classname='sign-up-display-div'>
-      <div classname='sign-up-div'>
-        <form classname='sign-up-form' onSubmit={(ev) => handleSubmit(ev)} >
-          <label classname='sign-up-name-label'>Name:</label>
-          <input classname='sign-up-name-input' value={name} onChange={(ev) => setName(ev.target.value)} />
+    <div classname="sign-up-display-div">
+      <div classname="sign-up-div">
+        <form classname="sign-up-form" onSubmit={(ev) => handleSubmit(ev)}>
+          <label classname="sign-up-name-label" htmlFor="sign-up-name-input">
+            Name:
+          </label>
+          <input
+            classname="sign-up-name-input"
+            id="sign-up-name-input"
+            type="text"
+            value={name}
+            placeholder="name"
+            onChange={(ev) => setName(ev.target.value)}
+            required
+          />
           <br />
-          <label classname='sign-up-email-label'>Email:</label>
-          <input classname='sign-up-email-input' value={email} onChange={(ev) => setEmail(ev.target.value)} />
+
+          <label classname="sign-up-email-label"
+            htmlFor="sign-up-email-input">
+            Email:
+          </label>
+          <input
+            classname="sign-up-email-input"
+            id="sign-up-email-input"
+            type="email"
+            value={email}
+            placeholder="email"
+            onChange={(ev) => setEmail(ev.target.value)}
+            required
+          />
           <br />
-          <label classname='sign-up-password-label'>Password:</label>
-          <input classname='sign-up-password-input' value={password} onChange={(ev) => setPassword(ev.target.value)} />
+
+          <label
+            classname="sign-up-password-label"
+            htmlFor="sign-up-password-input"
+          >
+            Password:
+          </label>
+          {/* update type to password once testing complete */}
+          <input
+            classname="sign-up-password-input"
+            id="sign-up-password-input"
+            type="text"
+            value={password}
+            placeholder="********"
+            onChange={(ev) => setPassword(ev.target.value)}
+            required
+          />
           <br />
-          <label classname='sign-up-confirm-label'>Confirm Password:</label>
-          <input classname='sign-up-confirm-input' value={confirm} onChange={(ev) => handleChange(ev)} />
+
+          <label
+            classname="sign-up-confirm-label"
+            htmlFor="sign-up-confirm-input"
+          >
+            Confirm Password:
+          </label>
+          {/* update type to password once testing complete */}
+          <input
+            classname="sign-up-confirm-input"
+            id="sign-up-confirm-input"
+            type="text"
+            value={confirm}
+            placeholder="********"
+            onChange={(ev) => handleChange(ev)}
+            required
+          />
           <br />
-          {matchToggle ? <div classname='password-match-div'>Passwords must match</div> : null}
-          {lengthToggle ? <div classname='password-match-div'>Password must be at least 8 characters</div> : null}
+
+          {matchToggle ? (
+            <div classname="password-match-div">Passwords must match</div>
+          ) : null}
+          {lengthToggle ? (
+            <div classname="password-match-div">
+              Password must be at least 8 characters
+            </div>
+          ) : null}
           <br />
-          <input type='submit' value='Sign Up' />
+
+          <input type="submit" value="Sign Up" />
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
