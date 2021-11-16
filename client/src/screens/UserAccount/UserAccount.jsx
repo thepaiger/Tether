3ad946f6d7be4fda085a436aa221116e9221d577
tyrus <tyrushenry03@gmail.com>
2021-11-16 10:26:38 -0500

    layout added to useraccount

--- client/src/screens/UserAccount/UserAccount.jsx
+++ client/src/screens/UserAccount/UserAccount.jsx
@@ -1,19 +1,23 @@
+import { Layout } from "../../components";
+
 const UserAccount = ({ user }) => {
   return (
-    <div className="account-info">
-      {user && <div className="account-details">
-        <p className="account-form">Name:</p>
-        <p className="account-name">{user.name}</p></div>}
+    <Layout>
+      <div className="account-info">
+        {user && <div className="account-details">
+          <p className="account-form">Name:</p>
+          <p className="account-name">{user.name}</p></div>}
 
-      {user && <div className="account-details">
-        <p className="account-form">Email:</p>
-        <p className="account-email">{user.email}</p></div>}
+        {user && <div className="account-details">
+          <p className="account-form">Email:</p>
+          <p className="account-email">{user.email}</p></div>}
 
-      <div className="account-details">
-        <p className="account-form">Password:</p>
-        <p className="account-password">********</p>
+        <div className="account-details">
+          <p className="account-form">Password:</p>
+          <p className="account-password">********</p>
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
