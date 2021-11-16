400380ddf55bdedcb916eb679b0fbd0eec7316e9
Darryl Falls <darryl.a.falls@gmial.com>
2021-11-16 13:48:56 -0500

    for pull down

--- client/src/screens/ShoppingCart/ShoppingCart.jsx
+++ client/src/screens/ShoppingCart/ShoppingCart.jsx
@@ -1,8 +1,16 @@
+import Layout from "../../components/Layout/Layout"
+import {useState} from 'react'
 
 
-const ShoppingCart = ({ }) => {
+const ShoppingCart = ({user, setUser}) => {
+  const [total, setTotal] = useState(0)
   return (
-    <div></div>
+    <Layout>
+      <div>
+        
+      </div>
+      <div> Total: ${total}</div>
+    </Layout>
   )
 }
 
