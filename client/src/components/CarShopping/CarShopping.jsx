3ff3ea9c7833b5e615cee5d324c453bfb4dc5e83
Darryl Falls <darryl.a.falls@gmial.com>
2021-11-16 17:39:21 -0500

    updating shoppingCart

--- client/src/components/CarShopping/CarShopping.jsx
+++ client/src/components/CarShopping/CarShopping.jsx
@@ -1,13 +1,59 @@
 import "./CarShopping.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { verifyUser, updateUser } from "../../services/users";
 
-const CarShopping = ({car, car_id, price, item_id, quantity, image}) => {
-  
+const CarShopping = ({car, car_id, price, priceNum, item_id, quantity, image, user, idx, setUser}) => {
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    setInput(quantity)
+  }, [quantity])
 
   const getTotal = () => {
-    let total = price * quantity;
+    let total = priceNum * quantity;
     return total;
+  }
+
+  const resetInput = () => {
+    setInput(quantity)
+  }
+
+  const getInput = (ev) => {
+    setInput(ev.target.value)
+  }
+
+  const remove = async () => {
+    const tempCart = user.shopping_cart
+    tempCart.splice(idx, 1)
+    const newCart = {
+      shopping_cart: tempCart
+    }
+    updateUser(user._id, newCart)
+    const updatedUser = await verifyUser()
+    setUser(updatedUser)
+  }
+
+  const editCart = (ev) => {
+    const tempCart = user.shopping_cart
+    tempCart[idx].quantity = ev
+    const newCart = {
+      shopping_cart: tempCart
+    }
+    updateUser(user._id, newCart)
+    const updatedUser = await verifyUser()
+    setUser(updatedUser)
+  }
+
+  const handleChange = (ev) => {
+    getInput(ev)
+    if (ev < 0 || ev.includes('e') || ev.includes('.')) {
+      resetInput()
+    } else if (ev === 0) {
+      remove()
+    } else {
+      editCart(ev)
+    }
   }
 
   return (
@@ -34,14 +80,13 @@ const CarShopping = ({car, car_id, price, item_id, quantity, image}) => {
           <input
             type="number"
             name="quantity"
-            value={quantity}
-            onChange={(ev) => setQuantity(ev.target.value)}
+            value={input}
+            onChange={handleChange}
             required
           />
           <h6>quantity</h6>
         </div>
         <div className="carShopping-total-div">
-          {/* ADD LOGIC TO MULTIPLY HERE */}
           <h4>${getTotal}</h4>
           <h6>total</h6>
         </div>
