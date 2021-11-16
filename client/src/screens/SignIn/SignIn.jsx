fdcfa46bf72f135309adc67d74ded495c4cdbbf6
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-11-16 11:13:24 -0600

    SignIn CSS almost complete

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -32,44 +32,45 @@ const SignIn = ({ user, setUser, loggedIn, setLoggedIn }) => {
 
   return (
     <Layout>
-      <div className="sign-in-display-div">
-        <div className="sign-in-div">
-          <form className="sign-in-form" onSubmit={(ev) => handleSubmit(ev)}>
-            <h2>Sign In</h2>
+      <div className="sign-in-background-img">
+        <div className="sign-in-display-div">
+          <div className="sign-in-div">
+            <form className="sign-in-form" onSubmit={(ev) => handleSubmit(ev)}>
+              <h2>Sign In</h2>
 
-            <div className="sign-in-fields">
-              <label className="sign-in-email-label" htmlFor="sign-in-email-input">Email:</label>
-              <input
-                className="sign-in-email-input"
-                id="sign-in-email-input"
-                type="email"
-                value={email}
-                placeholder="email"
-                onChange={(ev) => setEmail(ev.target.value)}
-                required
-              />
-              <label className="sign-in-password-label" htmlFor="sign-in-password-input">Password:</label>
+              <div className="sign-in-fields">
+                <label className="sign-in-email-label" htmlFor="sign-in-email-input">Email:</label>
+                <input
+                  className="sign-in-email-input"
+                  id="sign-in-email-input"
+                  type="email"
+                  value={email}
+                  placeholder="email"
+                  onChange={(ev) => setEmail(ev.target.value)}
+                  required
+                />
+                <label className="sign-in-password-label" htmlFor="sign-in-password-input">Password:</label>
 
-              {/* update type to password once testing complete */}
-              <input
-                className="sign-in-password-input"
-                id="sign-in-password-input"
-                type="text"
-                value={password}
-                placeholder="********"
-                onChange={(ev) => setPassword(ev.target.value)}
-                required
-              />
-            </div>
+                {/* update type to password once testing complete */}
+                <input
+                  className="sign-in-password-input"
+                  id="sign-in-password-input"
+                  type="text"
+                  value={password}
+                  placeholder="********"
+                  onChange={(ev) => setPassword(ev.target.value)}
+                  required
+                />
+              </div>
 
-            <input type="submit" value="Sign In" />
+              <input className="sign-in-submit" type="submit" value="Submit" />
 
-            <div>
-              <div>Don't have an account? <Link to='/signUp'>Sign Up</Link></div>
-            </div>
-          </form>
+              <div className="no-account">
+                <p>Don't have an account? <Link to='/signUp'>Sign Up</Link></p>
+              </div>
+            </form>
+          </div>
         </div>
-
       </div>
     </Layout>
   );
