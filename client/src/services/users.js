34296ec53d8fed94c3b53bb175e671b413f9e312
brianna groden <briannaggroden@gmail.com>
2021-11-17 12:17:21 -0500

    userupdate navigate works on submit

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -70,7 +70,7 @@ export const verifyUser = async () => {
 
 export const getUser = async id => {
   try {
-    const response = await api.get(`/users/${id}`)
+    const resp = await api.get(`/users/${id}`)
     localStorage.setItem('token', resp.data.token)
     const user = jwtDecode(resp.data.token)
     return user
