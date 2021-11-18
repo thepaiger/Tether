5c85ff6deb66b61bf759cc70ad99cc46b0213ab2
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-11-18 14:00:16 -0600

    fixed github link, changed title from 'React App' to 'Tether'

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,14 +1,13 @@
 import './Footer.css'
-import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer>
       <p className="footer-copyright">Copyright © 2021</p>
 
-      <NavLink to="https://github.com/DarrylFalls/Tether" className="footer-github">
+      <a className="footer-github" href="https://github.com/DarrylFalls/Tether">
         <img src={'/images/icons/github.svg'} alt='github link'></img>
-      </NavLink>
+      </a>
     </footer >
   )
 }
