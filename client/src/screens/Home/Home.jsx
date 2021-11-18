6251b0f00db36322cf1f6d07889ecebc1793b8f4
tyrus <tyrushenry03@gmail.com>
2021-11-18 14:20:11 -0500

    home page populated

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -6,7 +6,17 @@ import { NavLink } from 'react-router-dom'
 const Home = (props) => {
   return (
     <Layout user={props.user} setUser={props.setUser}>
-      {/* <ControlledCarousel /> */}
+      <div className="home-background-image">
+        {/* <ControlledCarousel /> */}
+        <div className='welcome-browse'>
+          <h1>WELCOME TO THE FUTURE</h1>
+          <NavLink to="/cars">Browse Inventory</NavLink>
+        </div>
+        <div className="home-logo">
+          <img src={'/images/logo.png'} alt="Logo" className="home-logo" />
+          <p>Your one-stop shop for Electric Supercars</p>
+        </div>
+      </div>
     </Layout>
   )
 }
