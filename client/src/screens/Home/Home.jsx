b91bce9b4e180a09603ba804c377a8fbf092a46c
Mike <mnezz1131@gmail.com>
2021-11-19 09:38:41 -0600

    Image Slider/CarDetails gallery functional

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = (props) => {
         {/* <ControlledCarousel /> */}
         <div className='welcome-browse'>
           <h1>WELCOME TO THE FUTURE</h1>
-          <NavLink to="/cars">Browse Inventory</NavLink>
+          {/* <NavLink to="/cars">Browse Inventory</NavLink> */}
         </div>
         <div className="home-logo">
           <img src={'/images/logo.png'} alt="Logo" className="home-logo" />
