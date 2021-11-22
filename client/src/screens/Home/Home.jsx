e0edea3c082b3d512bd4a2245278e77e5d76840f
Darryl Falls <darryl.a.falls@gmial.com>
2021-11-22 09:13:38 -0500

    updating remote branch

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -2,7 +2,6 @@ import './Home.css'
 import { Layout } from '../../components'
 import { NavLink } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import ControlledCarousel from '../../components/Carousel/Carousel'
 
 
 
@@ -10,8 +9,8 @@ const Home = (props) => {
   const [count, setCount] = useState(0)
   const [imgUrl, setImgUrl] = useState("url(/images/backgrounds/rimac-home-page.jpeg)")
   const images = [
-    '/images/backgrounds/2021-porsche-taycan.jpeg',
-    '/images/backgrounds/lamborghini-terzo-millennio.jpg',
+    '/images/backgrounds/Aston-Martin.jpg',
+    '/images/backgrounds/Terzo_Millennio.jpg',
     '/images/backgrounds/rimac-home-page.jpeg'
   ]
   
@@ -40,13 +39,12 @@ const Home = (props) => {
     setTimeout(() => {
       changeCount()
       changeImage()
-    }, 2000)
+    }, 4000)
   })
     
   return (
     <Layout user={props.user} setUser={props.setUser}>
       <div className="home-background-image" style={{ backgroundImage: imgUrl }}>
-        {/* <ControlledCarousel > */}
         <div className='home-content'>
           <div className="welcome-content">
             <h1 className="welcome-text">WELCOME TO THE FUTURE</h1>
@@ -57,12 +55,9 @@ const Home = (props) => {
             <p className="logo-subtext">YOUR ONE-STOP SHOP FOR ELECTRIC SUPERCARS</p>
           </div>
         </div>
-        {/* </ControlledCarousel> */}
       </div>
     </Layout>
   )
 }
 
 export default Home;
-
-// style={{ backgroundImage: imgUrl }}
